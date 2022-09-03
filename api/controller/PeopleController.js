43f6a96fbe1295ed44e59fde4ce76dffcdf95dfd
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-09-02 23:12:49 -0300

    implementing CRUD for People database

--- api/controller/PeopleController.js
+++ api/controller/PeopleController.js
@@ -10,6 +10,51 @@ class PeopleController {
         }
         
     }
+
+    static async getPersonById(req, res) {
+        const { id } = req.params
+        try {
+            const person = await database.People.findOne( { where: { id: Number(id) }})
+            res.status(200).json(person)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async createPerson(req, res) {
+        const newPerson = req.body
+        try {
+            const newPersonCreated = await database.People.create(newPerson);
+            res.status(200).json(newPersonCreated)
+
+        } catch (error) {
+            res.status(500).json(error.message)
+        }
+    }
+
+    static async updatePerson(req, res) {
+        const { id } = req.params
+        const updateData = req.body
+        try {
+            await database.People.update(updateData, { where: { id: Number(id) } })
+            const updatedPerson = await database.People.findOne( { where: { id: Number(id) } })
+            res.status(201).json(updatedPerson);
+        } catch (error) {
+            res.status(500).json(error.message)
+        }
+    }
+
+    static async deletePerson(req, res) {
+        const { id } = req.params
+        try {
+            await database.People.destroy( { where: { id: Number(id) } });
+            res.status(201).json({ message: "Person deleted!" })  
+        } catch (error) {
+            res.status(500).json(error.message)
+        }
+    }
 }
+
+
 
 module.exports = PeopleController;
--- api/routes/peopleRoute.js
+++ api/routes/peopleRoute.js
@@ -5,5 +5,9 @@ const PeopleController = require('../controller/PeopleController');
 const router = Router();
 
 router.get('/people', PeopleController.getAllPeople)
+router.get('/people/:id', PeopleController.getPersonById)
+router.post('/people', PeopleController.createPerson)
+router.put('/people/:id', PeopleController.updatePerson)
+router.delete('/people/:id', PeopleController.deletePerson)
 
 module.exports = router
