6aa5020ec2a72561f4dc3c3669ea662a3c86e6e5
Gabriel Baptista Cerqueira <66979159+gbcbaptista@users.noreply.github.com>
2022-09-28 18:47:22 -0300

    taking out unnecessary code/files

--- api/controller/StudentGroupsController.js
+++ api/controller/StudentGroupsController.js
@@ -1,15 +1,32 @@
 const database = require('../models');
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 class StudentGroupsController {
     static async getAllStudentGroups(req, res) {
+        const { start_date, end_date } = req.query
+        const where = {}
+        start_date || end_date ? where.start_date = {} : null
+        start_date ? where.start_date[Op.gte] = start_date : null
+        end_date ? where.start_date[Op.lte] = end_date : null
+        console.log(where)
         try {
-            const allStudentGroups = await database.StudentGroups.findAll()
+            const allStudentGroups = await database.StudentGroups.findAll({ where })
             return res.status(200).json(allStudentGroups);
         } catch (error) {
             return res.status(500).json(error.message);
         }
-        
     }
+
+    // static async getAllStudentGroups(req, res) {
+    //     try {
+    //         const allStudentGroups = await database.StudentGroups.findAll()
+    //         return res.status(200).json(allStudentGroups);
+    //     } catch (error) {
+    //         return res.status(500).json(error.message);
+    //     }
+        
+    // }
 
     static async getStudentGroupById(req, res) {
         const { id } = req.params
